f88996e43ddaaffa3f742174589a6ea920369094
duhber <duhbera@gmail.com>
2016-02-27 01:24:33 +0530

    add settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,8 @@
-HOME_PAGE="https://kissanime.to"
-LOGIN_PAGE="https://kissanime.to/Login"
+HOME_PAGE = "https://kissanime.to"
+LOGIN_PAGE = "https://kissanime.to/Login"
+DESTINATION = "/Users/duhber/python/kissdownloader/"
+VIDEO_URLS = "video_urls"
+DOWNLOAD_LINKS = "download_links"
+USERNAME = "duhbera"
+PASSWORD = "dewey0921"
+VIDEO_QUALITY=["480x360.mp4","540x360.mp4","640x360.mp4"]
